153426b2379ecd42fd56ae3eeec0bd956ac7b20b
Lukas F <lukas.forst48@googlemail.com>
2018-09-08 14:36:16 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,16 @@
 def turn_count(board)
-  counter = 0
-  board.each do |turn|
-    if turn == "X" || turn == "O"
-      counter += 1
+  # board.count{|token| token == "X" || token == "O"}
+
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
     end
   end
-  counter
+  turns
+
 end
 
 def current_player(board)
-  input_type = turn_count(board) % 2 == 0 ? "X" : "O"
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
